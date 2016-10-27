ebc414e5244fe097565ca4c1b9459d4d38c42203
roblad <roblad@users.noreply.github.com>
2016-10-27 20:58:19 +0200

    change mqtt server and description - private server

--- arduino/gps-tracker2/config.h
+++ arduino/gps-tracker2/config.h
@@ -1,14 +1,24 @@
+/*define mqtt brocker 
+ * https://customer.cloudmqtt.com/login
+ * register your server (for free 10 connections allowed)
+ * set direct access to server - config as standard owntrack config
+ * create user i.e. piesek
+ * create topic for user RW access owntracks/#
+ * 
+ * 
+ */
+
 #define GSM_APN         "internet"
 #define GSM_USERNAME    ""
 #define GSM_PASSWORD    ""
 
-#define AIO_SERVER      "broker.mqttdashboard.com"
-#define AIO_SERVERPORT  1883
-// #define AIO_USERNAME    ""
-// #define AIO_PASSWORD    ""
+#define AIO_SERVER      "xxxxxx.cloudmqtt.com"
+#define AIO_SERVERPORT  11971
+#define AIO_USERNAME    "xxxxxxx"
+#define AIO_PASSWORD    "xxxxxxx"
 
 #define TOPIC           "owntracks/piesek/yasmina"
 #define TID             "yb" // "tracker-ID which is used by the auto-faces feature to display, say, initials of a user."
 #define SPEED_TR        2  // bellow that number = 0
 
-#define DELAY           10  // seconds
+#define DELAY           5  // seconds
