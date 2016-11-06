16204bad37e9c3d5f812e4f30bfbd4c98c69989b
roblad <roblad@users.noreply.github.com>
2016-11-06 17:05:08 +0100

    own server added

--- arduino/gps-tracker2/config.h
+++ arduino/gps-tracker2/config.h
@@ -8,17 +8,30 @@
  * 
  */
 
-#define GSM_APN         "internet"
-#define GSM_USERNAME    ""
-#define GSM_PASSWORD    ""
+#define GSM_APN "internet"
+#define GSM_USERNAME ""
+#define GSM_PASSWORD ""
 
-#define AIO_SERVER      "xxxxxx.cloudmqtt.com"
-#define AIO_SERVERPORT  11971
-#define AIO_USERNAME    "xxxxxxx"
-#define AIO_PASSWORD    "xxxxxxx"
 
-#define TOPIC           "owntracks/piesek/yasmina"
-#define TID             "yb" // "tracker-ID which is used by the auto-faces feature to display, say, initials of a user."
-#define SPEED_TR        2  // bellow that number = 0
+//#define SUBSCRIPTIONDATALEN 122
 
-#define DELAY           5  // seconds
+
+
+//#define AIO_SERVER      "m20.cloudmqtt.com"
+
+//#define AIO_SERVER      "broker.mqttdashboard.com"
+//#define AIO_SERVER "broker.hivemq.com"
+#define AIO_SERVER "155.133.46.194"
+#define AIO_SERVERPORT 1883
+//#define AIO_SERVERPORT  11971
+//#define AIO_SERVERPORT  10429
+#define AIO_USERNAME    "xxxxxxxxxxx"
+#define AIO_PASSWORD    "xxxxxxxxxxx"
+//#define AIO_USERNAME    "gosecwhh"
+//#define AIO_PASSWORD    "OQoeB0JUxk22"
+
+#define TOPIC "owntracks/piesek/yasmina"
+#define TID "yb" // "tracker-ID which is used by the auto-faces feature to display, say, initials of a user."
+#define SPEED_TR 2  // bellow that number = 0
+#define HEARTBIT 220 // there ic multiply by DELAY and sending heartbit - set for every 30 min when movement trying is 5 sec.
+#define DELAY 7  // seconds
