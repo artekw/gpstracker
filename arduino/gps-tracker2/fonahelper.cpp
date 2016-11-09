c2fced42553fb2833f5523fc294611208728411c
roblad <roblad@users.noreply.github.com>
2016-11-09 07:51:28 +0100

    last but not well - 714 byte ram - not help

--- arduino/gps-tracker2/fonahelper.cpp
+++ arduino/gps-tracker2/fonahelper.cpp
@@ -9,7 +9,14 @@
 
 extern Adafruit_FONA fona;
 extern SoftwareSerial fonaSS;
-extern SoftwareSerial *fonaSerial;
+//extern SoftwareSerial *fonaSerial;
+
+
+
+
+
+
+
 
 
 
@@ -17,19 +24,22 @@ extern SoftwareSerial *fonaSerial;
 byte gps_enabled = 0;
 
 
+
+
 //send AT command and wait for responce and print
+
 
 uint8_t sendATcommand(char* ATcommand,unsigned int *timeout){
 
     uint8_t x=0,  answer=0;
-    char response[30];
+    char response[20];
     unsigned long previous;
 
-    while( fonaSerial->available() > 0) fonaSerial->read();    // Clean the input buffer
+    while( fonaSS.available() > 0) fonaSS.read();    // Clean the input buffer
     fonaSS.flush();
     if (ATcommand[0] != '\0')
     {
-        fonaSerial->println(ATcommand);    // Send the AT command 
+        fonaSS.println(ATcommand);    // Send the AT command 
         delay(20);
     }
 
@@ -38,13 +48,15 @@ uint8_t sendATcommand(char* ATcommand,unsigned int *timeout){
     previous = millis();
     Serial.println();
     Serial.print(F("Execute AT command: "));
+    delay(10);
     Serial.println(ATcommand);
 
     Serial.print(F("Result: "));
+    delay(10);
     // this loop waits for the answer
     do{
-        if(fonaSerial->available() > 0){    // if there are data in the UART input buffer, reads it and checks for the asnwer
-            response[x] = fonaSerial->read();
+        if(fonaSS.available() > 0){    // if there are data in the UART input buffer, reads it and checks for the asnwer
+            response[x] = fonaSS.read();
             
             Serial.print(response[x]);
             //delay(20);
@@ -64,21 +76,24 @@ uint8_t sendATcommand(char* ATcommand,unsigned int *timeout){
 
 boolean FONAconnect(const __FlashStringHelper *apn, const __FlashStringHelper *username, const __FlashStringHelper *password) {
   //Watchdog.reset();
-  fonaSerial->begin(9600);
-  if (! fona.begin(*fonaSerial)) {           // can also try fona.begin(Serial1) 
+  fonaSS.begin(9600);
+  if (! fona.begin(fonaSS)) {           // can also try fona.begin(Serial1) 
     Serial.println(F("Couldn't find FONA"));
+    delay(10);
     return false;
   } else {
   //fona.deleteSMS(1);
-  
+  //keep alive tcp 
+//  fonaSS.println("AT&V",500);
+  //delay(200);
   //Serial.print(F("Check FONA err: - expect OK"));
  //set additionalcommands to modem  
- //   sendATcommand("AT+CMEE=2", 200);
+   //sendATcommand("AT+CGATT=1",1000);
 //  Serial.print(F("Delete all SMS - expect OK"));
 //  sendATcommand("AT+CMGD=1,4",200);
    //sendATcommand("AT&W",200);
    //Serial.print(F("Registration IP - expect OK"));
-   //sendATcommand("AT+SAPBR=2,1",200);
+   //sendATcommand("AT+CGMSCLASS=12",200);
    //reset GPS
    //led blink 0 1 2
    //AT+CSGS=2
@@ -94,10 +109,15 @@ boolean FONAconnect(const __FlashStringHelper *apn, const __FlashStringHelper *u
    //"AT&W";
    //delay(1000);
   }
+
   
   while (! fona.getNetworkStatus()) {
    Serial.println(F("Waiting for network..."));
-    delay(1000);
+   delay (10);
+
+
+
+    //delay(1000);
     /*   
         uint8_t n = fona.getRSSI();
         int8_t r;
@@ -111,27 +131,33 @@ boolean FONAconnect(const __FlashStringHelper *apn, const __FlashStringHelper *u
         }
    Serial.print(r); Serial.println(F(" dBm"));
    
+
+
+
    
+
    */
            
    //Watchdog.reset();
   }
 
   //Watchdog.reset();
-  delay(7000);  // wait a few seconds to stabilize connection
+  delay(10000);  // wait a few seconds to stabilize connection
   //Watchdog.reset();
   fona.setGPRSNetworkSettings(apn, username, password);
-  delay(20);
+  delay(50);
   //Serial.println(F("Disabling GPRS"));
   fona.enableGPRS(false);
+
   //Watchdog.reset();
-  delay(10000);  // wait a few seconds to stabilize connection
+  delay(5000);  // wait a few seconds to stabilize connection
   //Watchdog.reset();
 
   Serial.println(F("Enabling GPRS"));
   while (!fona.enableGPRS(true)) {
-    delay(10000);
+    delay(50);
     Serial.println(F("Failed to turn GPRS on"));  
+    delay(10);
     
     return false;
   }
@@ -145,8 +171,10 @@ boolean FONAconnect(const __FlashStringHelper *apn, const __FlashStringHelper *u
 boolean GPS() {
   if (gps_enabled == 0 ) {
     Serial.println(F("Enabling GPS"));
+    delay(10);
     if (!fona.enableGPS(true)) {
-      Serial.println(F("Failed to turn GPS on"));  
+      Serial.println(F("Failed to turn GPS on"));
+      delay(10);  
 
       return false;
     }
@@ -157,23 +185,31 @@ boolean GPS() {
 
   //Watchdog.reset();
   delay(5000);  // wait a few seconds to stabilize connection
+
+
   //Watchdog.reset();
+
   byte stat;
   stat = fona.GPSstatus();
   
-  if (stat < 0 or stat == 0  or stat == 1) {
+  if (&stat < 0 or &stat == 0  or &stat == 1) {
     
     Serial.println(F("Not fixed!"));
+    delay(10);
 
     return false;
     //delay(5000);
   }
   if (stat >= 2) {
     Serial.println(F("GPS Fixed"));
+    delay(10);
   }
+
   //Watchdog.reset();
+
   return true;
 }
+
 
 void GPS_Data(float *fdata, int *idata) {
   char utime[14];
@@ -187,9 +223,10 @@ void GPS_Data(float *fdata, int *idata) {
   boolean gps_success = fona.getGPS(&fdata[0], &fdata[1], &fdata[3], &fdata[2], &fdata[4], &utime[0]);
   delay(20);
   sscanf(utime,"%04d%02d%02d%02d%02d%02d",&YY,&MM,&DD,&hh,&mm,&ss);
+  //sscanf(utime,"%04d%02d%02d%02d%02d%02d",&idata[5],&idata[4],&idata[3],&idata[0],&idata[1],&idata[2]);
   delay(20);
   idata[0] = hh;
-  idata[1] = mm;
+ idata[1] = mm;
   idata[2] = ss;
   idata[3] = DD;
   idata[4] = MM;
@@ -198,11 +235,13 @@ void GPS_Data(float *fdata, int *idata) {
   delay(20);
     
   if (&gps_success) {
+
+
+       //sendATcommand("AT+CGATT=1",1000);
+
     
     return &fdata;
     
   }
 }
-
-
 
