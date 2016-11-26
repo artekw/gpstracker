50b5b48da749a364cfdb58ee00181f1fd3485ccb
roblad <roblad@users.noreply.github.com>
2016-11-26 15:25:43 +0100

    final version

--- arduino/gps-tracker2/config.h
+++ arduino/gps-tracker2/config.h
@@ -8,7 +8,8 @@
  * 
  */
 //soft serial buffer
-#define _SS_MAX_RX_BUFF 128 
+#define _SS_MAX_RX_BUFF 128
+//gsm 
 #define GSM_APN "internet"
 #define GSM_USERNAME ""
 #define GSM_PASSWORD ""
@@ -18,17 +19,14 @@
 //#define AIO_SERVER      "m20.cloudmqtt.com"
 //#define AIO_SERVER      "broker.mqttdashboard.com"
 //#define AIO_SERVER "broker.hivemq.com"
-#define AIO_SERVER "155.133.46.194"
+#define AIO_SERVER "xxx.xxx.xxx.xxx"
 #define AIO_SERVERPORT 1883
-#define AIO_SERVER "155.133.46.194"
-#define AIO_SERVERPORT 1883
-//#define AIO_SERVERPORT  11971
-//#define AIO_SERVERPORT  10429
-#define AIO_USERNAME    "xxxxxxxxxxx"
-#define AIO_PASSWORD    "xxxxxxxxxxx"
+#define AIO_USERNAME    "piesek"
+#define AIO_PASSWORD    "xxxxxxxxx"
 #define TOPIC "owntracks/piesek/yasmina"
 #define TID "yb" // "tracker-ID which is used by the auto-faces feature to display, say, initials of a user."
 #define SPEED_TR 3  // bellow that number = 0
-#define SPEED_TR 3  // bellow that number = 0
-#define HEARTBIT 90 // there ic multiply by DELAY and sending heartbit - set for every 30 min when movement trying is 5 sec.
-#define DELAY 13 // seconds + ~6 
+#define HEARTBIT 120 // there ic multiply by DELAY and sending heartbit - set for every 30 min when movement trying is 5 sec.
+#define DELAY 10 // seconds + ~5 
+#define MOVEMENT 50.0  //metters moved to report
+//#define DEBUG
